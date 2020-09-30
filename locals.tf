061e6e7f5c54c728d67001ef1c7772258219b173
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-09-30 16:37:40 -0300

    included locals.tf for configure vpc

---
+++ locals.tf
@@ -0,0 +1,18 @@
+locals {
+  vpc_name = "${terraform.workspace == "dev"? "vpc-dev" : "vpc-prod"}"
+}
+
+resource "aws_vpc" "my_vpc" {
+  count = "${terraform.workspace == "dev"? 0:1}"
+  cidr_block = "${var.vpc_cidr}"
+  instance_tenancy = "default"
+  tags = {
+    Name = "${local.vpc_name}"
+    Environment = "${terraform.workspace}"
+    Location = "India"
+  }
+}
+
+//output "vpc_cidr" {
+//  value = "${aws_vpc.my_vpc.cidr_block}"
+//}
--- providers.tf
+++ providers.tf
@@ -2,20 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "my_vpc" {
-  count = 0
-  cidr_block = "${var.vpc_cidr}"
-  instance_tenancy = "default"
-  tags = {
-    Name = "terraform-vpc"
-    Environment = "${terraform.workspace}"
-    Location = "India"
-  }
-}
-
-//output "vpc_cidr" {
-//  value = "${aws_vpc.my_vpc.cidr_block}"
-//}
 
 
 terraform {
