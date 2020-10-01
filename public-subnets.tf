b0dc0b639231aaa445572d24c968b124463793e6
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-10-01 16:27:49 -0300

    improviments

--- public-subnets.tf
+++ public-subnets.tf
@@ -13,3 +13,21 @@ resource "aws_subnet" "public" {
     Name = "PublicSubnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.my_app.id}"
+  Name   = "terraform-igw"
+}
+
+resource "aws_route_table" "prt" {
+  vpc_id = ""
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags = {
+    Name = "terraform-prt"
+  }
+}
